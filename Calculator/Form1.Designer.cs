c6e559f78a0ee8bc0b0b8a4edbd4dba2813c6bc2
yuuma-yoshida <yuuma.yoshida.work@gmail.com>
2024-09-09 19:22:13 +0900

    電卓のフォームの実装を行いました。

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,12 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            richTextBox1 = new RichTextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 129);
+            button1.Name = "button1";
+            button1.Size = new Size(83, 54);
+            button1.TabIndex = 0;
+            button1.Text = "AC";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(72, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 1;
+
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(39, 34);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(423, 61);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(379, 129);
+            button2.Name = "button2";
+            button2.Size = new Size(83, 54);
+            button2.TabIndex = 4;
+            button2.Text = "÷";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(150, 215);
+            button3.Name = "button3";
+            button3.Size = new Size(83, 54);
+            button3.TabIndex = 5;
+            button3.Text = "8";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(39, 215);
+            button4.Name = "button4";
+            button4.Size = new Size(83, 54);
+            button4.TabIndex = 6;
+            button4.Text = "7";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(39, 389);
+            button5.Name = "button5";
+            button5.Size = new Size(83, 54);
+            button5.TabIndex = 7;
+            button5.Text = "1";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(379, 215);
+            button6.Name = "button6";
+            button6.Size = new Size(83, 54);
+            button6.TabIndex = 8;
+            button6.Text = "×";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(260, 215);
+            button7.Name = "button7";
+            button7.Size = new Size(83, 54);
+            button7.TabIndex = 9;
+            button7.Text = "9";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(150, 389);
+            button8.Name = "button8";
+            button8.Size = new Size(83, 54);
+            button8.TabIndex = 10;
+            button8.Text = "2";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(260, 297);
+            button9.Name = "button9";
+            button9.Size = new Size(83, 54);
+            button9.TabIndex = 11;
+            button9.Text = "6";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(150, 297);
+            button10.Name = "button10";
+            button10.Size = new Size(83, 54);
+            button10.TabIndex = 12;
+            button10.Text = "5";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(39, 297);
+            button11.Name = "button11";
+            button11.Size = new Size(83, 54);
+            button11.TabIndex = 13;
+            button11.Text = "4";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(260, 389);
+            button12.Name = "button12";
+            button12.Size = new Size(83, 54);
+            button12.TabIndex = 14;
+            button12.Text = "3";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(379, 389);
+            button13.Name = "button13";
+            button13.Size = new Size(83, 54);
+            button13.TabIndex = 15;
+            button13.Text = "＋";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(39, 474);
+            button14.Name = "button14";
+            button14.Size = new Size(194, 54);
+            button14.TabIndex = 16;
+            button14.Text = "0";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(260, 474);
+            button15.Name = "button15";
+            button15.Size = new Size(83, 54);
+            button15.TabIndex = 17;
+            button15.Text = ".";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(379, 474);
+            button16.Name = "button16";
+            button16.Size = new Size(83, 54);
+            button16.TabIndex = 18;
+            button16.Text = "=";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(379, 297);
+            button17.Name = "button17";
+            button17.Size = new Size(83, 54);
+            button17.TabIndex = 19;
+            button17.Text = "ー";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(502, 558);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(richTextBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private RichTextBox richTextBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
     }
 }
